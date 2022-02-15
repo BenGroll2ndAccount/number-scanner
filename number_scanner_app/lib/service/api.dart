32382e963db0c773d4bde82ea228711676b9d071
BenGrollPMT <bengroll002@gmail.com>
2022-02-15 11:51:58 +0100

    Changed Logo to ProMusicTools Logo

--- number_scanner_app/lib/service/api.dart
+++ number_scanner_app/lib/service/api.dart
@@ -3,25 +3,5 @@ import 'package:requests/requests.dart';
 import 'xmlparse.dart';
 import 'package:http/http.dart' as http;
 
-<<<<<<< HEAD
-dynamic sendsoaptest() async { 
-  
-=======
 dynamic getSoapResponseTest(context) async {
-  const bool output = false;
-  String xm = await DefaultAssetBundle.of(context).loadString("assets/contact.xml");  
-  var url = Uri.parse("https://www.promusictools.com/api/v2_soap/index?wsdl=1");
-  var response = await http.post(url, body : xm, headers: {"Content-Type" : "text/xml; charset=utf-8", "SOAPAction" : "AuthenticateCredential"});
-  if(output){
-  print('Response status: ${response.statusCode}');}
-  var rb = response.body;
-  var lines = rb.split("\n");
-  if(output){
-  for(int i = 0; i < ass.length; i++) {
-    print(lines[i]);
-  }
-  }
-   
-    
->>>>>>> 68e3997c79c4a7707bfdb31dbd050e0025a7a6d1
 }
--- number_scanner_app/lib/static/strings.dart
+++ number_scanner_app/lib/static/strings.dart
@@ -6,8 +6,8 @@ RichText getAppText(context) {
         style: TextStyle(fontSize: MediaQuery.of(context).size.height / 15, color : Colors.black),
         children: const <TextSpan>[
           TextSpan(text: "Pro", style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold)),
-          TextSpan(text: "Check", style: TextStyle(color: Colors.red, fontWeight: FontWeight.bold)),
-          TextSpan(text: "Tool", style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold))
+          TextSpan(text: "Music", style: TextStyle(color: Colors.red, fontWeight: FontWeight.bold)),
+          TextSpan(text: "Tools", style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold))
         ]
       ));
 }
